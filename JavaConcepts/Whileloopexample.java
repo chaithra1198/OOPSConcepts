fd292c0914980a3bcb9559f393d0d37aa3252921
chaithra <chaitra123@gmail.com>
2024-02-12 16:48:18 +0530

    while loop example program

---
+++ JavaConcepts/Whileloopexample.java
@@ -0,0 +1,16 @@
+package JavaConcepts;
+
+public class Whileloopexample {
+
+	public static void main(String[] args) {
+		int i=10;
+		while(i<10)
+		{
+			System.out.println(i);
+			i--;
+		}
+		
+
+	}
+
+}
