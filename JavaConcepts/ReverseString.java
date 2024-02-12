6c27321bfbd716f6334a1d6aa79b20c1f543f03b
chaithra <chaitra123@gmail.com>
2024-02-12 17:08:39 +0530

    reverse string program

--- JavaConcepts/ReverseString.java
+++ JavaConcepts/ReverseString.java
@@ -3,9 +3,13 @@ package JavaConcepts;
 public class ReverseString {
 
 	public static void main(String[] args) {
-		String name="chaithra";
+		String name="Automation intervew questions";
 		
-		for(int i=name.length()-1;i>=0;i--)
+		/*for(int i=name.length()-1;i>=0;i--)
+		{
+			System.out.print(name.charAt(i));
+		}*/
+		for(int i=0;i<=name.length()-1;i++)
 		{
 			System.out.print(name.charAt(i));
 		}
